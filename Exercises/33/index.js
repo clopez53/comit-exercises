6c8baf0a61e0fb07ead854ab9a63211b9c537b8d
christ <c.james.lopez14@gmail.com>
2020-10-28 20:59:37 -0500

    Finished Exercise 33

---
+++ Exercises/33/index.js
@@ -0,0 +1,31 @@
+"use strict";
+
+let numbers = [1, 2, 3, 4, 5];
+
+let squaredNumbers = numbers.map(number => number*number);
+
+console.log(numbers);
+console.log(squaredNumbers);
+
+let moreNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let evenNumbers = moreNumbers.filter(number => number % 2 == 0);
+let oddNumbers = moreNumbers.filter(number => number % 2 != 0);
+
+console.log(moreNumbers);
+console.log(evenNumbers);
+console.log(oddNumbers);
+
+let people = [{name:"Tom", age:34, city:"Winnipeg"},
+              {name:"Dick", age:5, city:"Toronto"},
+              {name:"Harry", age:12, city:"Saskatoon"},
+              {name:"Justin", age:34, city:"London"},
+              {name:"Timberlake", age:34, city:"Columbia"}];
+
+let oldPeople = people.filter(person => person.age > 30);
+let youngSasks = people.filter(person => person.age < 20 && person.city == "Saskatoon");
+let ages = people.map(person => person.age);
+
+console.log(oldPeople);
+console.log(youngSasks);
+console.log(ages);
